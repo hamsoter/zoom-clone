6ecdabb6097d2c0022040836206f3e7e59779782
hamsoter <omilzmil0w0@gmail.com>
2022-03-23 05:19:53 +0900

    Feat:프론트단 닉네임 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 
 const backSocket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 backSocket.addEventListener("oepn", () => {
   console.log("Connented to Server👀");
@@ -9,6 +15,9 @@ backSocket.addEventListener("oepn", () => {
 
 backSocket.addEventListener("message", (message) => {
   console.log("메세지: ", message.data, " from the Server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 backSocket.addEventListener("close", () => {
@@ -18,9 +27,20 @@ backSocket.addEventListener("close", () => {
 const handleSubmit = (e) => {
   e.preventDefault();
   const input = messageForm.querySelector("input");
-  backSocket.send(input.value);
+  // backSocket.send(input.value);
+  backSocket.send(makeMessage("new_message", input.value));
+  console.log(input.value);
+  input.value = "";
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  backSocket.send(makeMessage("nickname", input.value));
+
   console.log(input.value);
   input.value = "";
 };
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
